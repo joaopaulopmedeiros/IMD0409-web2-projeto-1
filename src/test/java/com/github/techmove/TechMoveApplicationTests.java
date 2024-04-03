875839929723e292ec23a367d5eb443142aaddd0
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-03 19:44:57 -0300

    chore: setup project

---
+++ src/test/java/com/github/techmove/TechMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.techmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
