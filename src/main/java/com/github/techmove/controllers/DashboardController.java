0c3560df22fcb2095e1d7f8c0984b62eacd8ea64
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-05 23:51:49 -0300

    refactor: dashboard link

--- src/main/java/com/github/techmove/controllers/DashboardController.java
+++ src/main/java/com/github/techmove/controllers/DashboardController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController 
+public class DashboardController 
 {
-    @GetMapping("/")
-    public String index()
+    @GetMapping("/dashboard")
+    public String dashboard()
     {
-        return "index";
-    }         
+        return "dashboard";
+    }     
 }
