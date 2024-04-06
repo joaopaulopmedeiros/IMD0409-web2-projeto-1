4ed92f947212ff7e6ac213cfd6121b966bccf7b1
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-05 23:47:13 -0300

    style: dashboard

--- src/main/java/com/github/techmove/controllers/HomeController.java
+++ src/main/java/com/github/techmove/controllers/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController
     {
         return "index";
     }   
+    @GetMapping("/dashboard")
+    public String dashboard()
+    {
+        return "dashboard";
+    }       
 }
