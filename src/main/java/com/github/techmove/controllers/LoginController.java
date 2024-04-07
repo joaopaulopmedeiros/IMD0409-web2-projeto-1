618442d4fac23f1dd45ff1133da6957f509f9c15
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-07 13:39:10 -0300

    feat: ui links

--- src/main/java/com/github/techmove/controllers/LoginController.java
+++ src/main/java/com/github/techmove/controllers/LoginController.java
@@ -2,6 +2,9 @@ package com.github.techmove.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController 
@@ -11,4 +14,14 @@ public class LoginController
     {
         return "login";
     }    
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password) {        
+        return "redirect:/dashboard"; 
+    }   
+
+    @PostMapping("/logout")
+    public String logout() {        
+        return "redirect:/"; 
+    }       
 }
