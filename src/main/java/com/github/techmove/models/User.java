61c8c84fc3015b13271f3cdddc6bbc710d4e5ef5
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-09 12:22:24 -0300

    feat: create users table

--- src/main/java/com/github/techmove/models/User.java
+++ src/main/java/com/github/techmove/models/User.java
@@ -2,18 +2,27 @@ package com.github.techmove.models;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String salt;
     private String hash;
-    private List<Activity> activities;
+    //private List<Activity> activities;
     private double score;
-    private Guild guild;    
+    //private Guild guild;    
 }
