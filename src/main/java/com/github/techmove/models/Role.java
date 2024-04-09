fde0ff2c1ecadd0d66a7bf6096775ec4c097c9dc
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-09 12:34:52 -0300

    feat: create db tables

--- src/main/java/com/github/techmove/models/Role.java
+++ src/main/java/com/github/techmove/models/Role.java
@@ -1,11 +1,21 @@
 package com.github.techmove.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "roles")
 @Getter
 @Setter
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
 }
