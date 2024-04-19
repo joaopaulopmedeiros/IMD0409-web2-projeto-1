4ac1066d2b7d5a85ffddaf46d5a6a91eeab2d3f1
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-18 22:04:28 -0300

    feat: ranking by current user guild

--- src/main/java/com/github/techmove/services/RankingService.java
+++ src/main/java/com/github/techmove/services/RankingService.java
@@ -3,23 +3,23 @@ package com.github.techmove.services;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.github.techmove.infra.repositories.UserRepository;
-import com.github.techmove.models.Guild;
 import com.github.techmove.models.Ranking;
 
 @Service
-public class RankingService {
-
+public class RankingService 
+{
     @Autowired
     private UserRepository repository;
 
-    public Ranking getByCurrentDate() {
-        
+    public Ranking getByCurrentDate() 
+    {
         LocalDateTime now = LocalDateTime.now();
-        
-        var guild = Guild.builder().id(1L).name("IMD Guild").build();
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        var guild = repository.findByEmail(email).getGuild();
         var users = repository.findByGuild(guild.getId());
 
         return Ranking.builder()
@@ -28,6 +28,5 @@ public class RankingService {
             .guild(guild)
             .users(users)
             .build();
-    }
-    
+    }   
 }
